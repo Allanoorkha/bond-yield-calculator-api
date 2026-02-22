b468b145dac4775dafc3dc0b7c564089ff8678be
Altaf Pathan <altafpathan@Altafs-MacBook-Air-2.local>
2026-02-22 23:49:48 +0400

    Add health check root route

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
 import { BondModule } from './modules/bond/bond.module';
 
 @Module({
   imports: [BondModule],
+  controllers: [AppController],
 })
 export class AppModule { }
